f96de276c066af1becfc344829c19025b9821311
Buiukbek Navruzov <Buiukbek.Navruzov@itechart-group.com>
2022-06-07 12:22:27 +0500

    Changed some logics from comment

--- FactoryMethodTask/Program.cs
+++ FactoryMethodTask/Program.cs
@@ -9,32 +9,9 @@ namespace FactoryMethodTask
         public static void Main(string[] args)
         {
             ClientShape client = new ClientShape();
-            
-            IShape shape = client.GetRandomShape();
-       
-            switch (shape)
-            {
-                case Triangle triangle:
-                    triangle.Name = "Triangle";
-                    triangle.Color = "Red";
-                    triangle.X = 3;
-                    triangle.Y = 5;
-                    triangle.Z = 2;
-                    break;
-                case Circle circle:
-                    circle.Name = "Circle";
-                    circle.Color = "Green";
-                    circle.Radius = 5;
-                    break;
-                case Rectangle rectangle:
-                    rectangle.Name = "Rectangle";
-                    rectangle.Color = "Black";
-                    rectangle.X = 5;
-                    rectangle.Y = 3;
-                    break;
-            }
+            client.CreateShapes();
 
-            Console.WriteLine(shape);
         }
+        
     }
 }
